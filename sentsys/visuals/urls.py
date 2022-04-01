a4629973ee052b1be95e289c5e7f6d3bb72be3ad
Christalline Joie Borjal <borjalchristallinejoie@gmail.com>
2022-04-01 08:11:25 +0800

    added lda

--- sentsys/visuals/urls.py
+++ sentsys/visuals/urls.py
@@ -6,6 +6,12 @@ urlpatterns = [
     path('', views.visuals, name='visuals'),
     path('visuals', views.visuals, name='visuals'),
     path('data', views.data, name='data'),
-    path('lda', views.data, name='lda'),
+    path('lda', views.lda, name='lda'),
+    path('nouns0', views.nouns0, name='nouns0'),
+    path('nouns1', views.nouns1, name='nouns1'),
+    path('nouns2', views.nouns2, name='nouns2'),
+    path('adj0', views.adj0, name='adj0'),
+    path('adj1', views.adj1, name='adj1'),
+    path('adj2', views.adj2, name='adj2'),
     path('admin/', admin.site.urls),
 ]
--- sentsys/visuals/views.py
+++ sentsys/visuals/views.py
@@ -2,22 +2,61 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
     return HttpResponse("Hello world!")
+
 
 def upload(request):
     template = loader.get_template('up.html')
     return HttpResponse(template.render())
 
+
 def visuals(request):
     template = loader.get_template('visuals.html')
     return HttpResponse(template.render())
+
 
 def data(request):
     template = loader.get_template('data.html')
     return HttpResponse(template.render())
 
+
 def lda(request):
     template = loader.get_template('LDA.html')
     return HttpResponse(template.render())
 
+
+def nouns0(request):
+    template = loader.get_template('nouns0.html')
+    return HttpResponse(template.render())
+
+
+def nouns1(request):
+    template = loader.get_template('nouns1.html')
+    return HttpResponse(template.render())
+
+
+def nouns2(request):
+    template = loader.get_template('nouns2.html')
+    return HttpResponse(template.render())
+
+
+def adj0(request):
+    template = loader.get_template('adj0.html')
+    return HttpResponse(template.render())
+
+
+def adj1(request):
+    template = loader.get_template('adj1.html')
+    return HttpResponse(template.render())
+
+
+def adj2(request):
+    template = loader.get_template('adj2.html')
+    return HttpResponse(template.render())
+
+
+def preprocessed(request):
+    template = loader.get_template('preprocessed.html')
+    return HttpResponse(template.render())
