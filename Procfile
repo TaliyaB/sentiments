b2b22c24d6ca0446c5e6917b85b0ab993b9d9261
Christalline Joie Borjal <borjalchristallinejoie@gmail.com>
2022-04-04 16:07:24 +0800

    whitenoise

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
 web: gunicorn sentsys.wsgi --log-file -
-web: python sentsys/manage.py collectstatic
-web: gunicorn sentsys.wsgi
-web: python sentsys/manage.py runserver
--- sentsys/sentsys/settings.py
+++ sentsys/sentsys/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'visuals/templates/static'),
 ]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
